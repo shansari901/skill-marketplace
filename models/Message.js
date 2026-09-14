e446718825eab2b6d358c9fb64998e1c55621775
shansari901 <sareefulh6@gmail.com>
2026-09-14 09:00:46 +0530

    Add Message model for chat system

---
+++ models/Message.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+  senderId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  recipientId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now
+  },
+  read: {
+    type: Boolean,
+    default: false
+  }
+});
+
+module.exports = mongoose.model('Message', messageSchema);
